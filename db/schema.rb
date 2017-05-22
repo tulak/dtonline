9183a82a5e4b9136d8be0857eeaa82d1bc426405
radtom <tomalaradovan@gmail.com>
2017-05-22 15:32:25 +0200

    banner models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218171728) do
+ActiveRecord::Schema.define(version: 20170522130750) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,12 @@ ActiveRecord::Schema.define(version: 20170218171728) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "bigbanners", force: :cascade do |t|
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
@@ -60,6 +66,12 @@ ActiveRecord::Schema.define(version: 20170218171728) do
     t.string   "image"
     t.integer  "category_id"
     t.index ["category_id"], name: "index_posts_on_category_id"
+  end
+
+  create_table "smallbanners", force: :cascade do |t|
+    t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
